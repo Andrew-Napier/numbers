ef2a6798bff1be74f94115b5c7d4fb46fc438f30
andrew.napier <andrew.napier@runbox.com>
2022-08-06 14:56:45 +1000

    Parking this borked code

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,6 +1,5 @@
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
