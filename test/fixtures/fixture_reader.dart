94f0e7dab32d924c6c49dd501333185a3dff7ba5
andrew.napier <andrew.napier@runbox.com>
2022-09-22 16:27:37 +1000

    made tests OS agnostic

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,3 +1,8 @@
 import 'dart:io';
 
-String fixture(String name) => File('test\\fixtures\\$name').readAsStringSync();
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+
+//String fixture(String name) => File('test\\fixtures\\$name').readAsStringSync();
+String fixture(String name) =>
+    File(p.joinAll(["test", "fixtures", name])).readAsStringSync();
